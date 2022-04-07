1ceb5b2bf739397ac4c3129c7019b3531a581675
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-04-07 06:52:44 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
-#include <stdio.h> 
+#include <stdio.h>
+
 /**
  * main - main block
  *
@@ -7,6 +8,6 @@
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
- return (0);
+return (0);
 }
 
