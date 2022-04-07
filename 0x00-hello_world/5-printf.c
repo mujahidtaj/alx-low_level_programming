41450760e54a35b382236a29f3efd37bb3b19078
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-04-07 06:17:21 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,15 @@
-th proper grammar, but the outcome is a piece of art,\n");#include <stdio.h>
+nclude <stdio.h>
 /**
  * main - main block
  *
  * Return: 0
  */
+
 int main(void)
 
 {
 
-iuts("\"Programming is like building a multilingual puzzle");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 
 return (0);
 
