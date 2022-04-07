61bf3c17101c4e31a2ff31cf32f6ae09315492a3
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-04-07 06:13:38 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+th proper grammar, but the outcome is a piece of art,\n");#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Return: 0
+ */
+int main(void)
+
+{
+
+iuts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+
+}
