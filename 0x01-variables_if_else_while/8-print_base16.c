b1de9f1682400dbb5996918c508a71c47a00505e
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-04-08 15:41:54 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  main - Prints if number is positive, zero or negative
+ *  Return: Always (Success)
+ */
+
+int main(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+for (c = 'a'; c <= 'f'; c++)
+{
+putchar(c);
+
+}
+
+putchar('\n');
+return (0);
+}
