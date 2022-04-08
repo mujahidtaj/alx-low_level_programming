b18dff0e3a1ab4400306bd1cff702c125398ef4d
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-04-08 16:45:30 -0700

    11. The success combination in business is

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,21 +9,24 @@
 
 int main(void)
 {
-int c, i;
+int c, i, k;
 for (c = '0'; c <= '9'; c++)
 
 {
 for (i = '0'; i <= '9'; i++)
 {
-if (c < i)
+for (k = '0'; k <= '9'; k++)
+{
+if (c < i && i < k)
 {
 putchar(c);
 putchar(i);
-if (c != '8' || (c == '8' && i != '9'))
-
+putchar(k);
+if (c != '7')
 {
 putchar(',');
 putchar(' ');
+}
 }
 }
 }
