d980933fe12c449a1b051e677a3964dba82432f4
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-04-08 15:38:42 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  main - Prints if number is positive, zero or negative
+ *  Return: Always (Success)
+ */
+
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; c--)
+{
+
+putchar(c);
+
+}
+
+putchar('\n');
+return (0);
+}
