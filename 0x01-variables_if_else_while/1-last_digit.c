bec0f170f8bd925d3a207821b44278be05398b14
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-04-08 11:18:02 -0700

    1. The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,15 +18,19 @@ lastd = n % 10;
 if (lastd > 5)
 
 {
+
 printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
 
 }
+
 else if (lastd == 0)
 
 {
+
 printf("Last digit of %d is %d and is 0\n", n, lastd);
 
 }
+
 else if (lastd < 6 && lastd != 0)
 
 {
