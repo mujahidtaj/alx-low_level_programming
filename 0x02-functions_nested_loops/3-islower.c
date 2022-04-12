7cfcba10b8045ae998060bb2301a1945320c4acd
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-04-12 11:31:33 -0700

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+if (c > 'a' && c < 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
