62e2a043becc1402ecc88ac4d7e54f593941e783
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-04-12 08:30:46 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,22 @@
 #include "main.h"
 /**
-* print_alphabet - print alphabet lowercase
-* Return: Always 0.
- */
-
-void print_alphabet(void)
+* print_alphabet - print alphabet low
+* * Return: Always 0.
+*/
+void print_alphabet_x10(void)
 {
-char alphabet;
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+int alphabet;
+int count;
+count = 0;
+while (count < 10)
+{
+for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
 {
 _putchar(alphabet);
 }
-count++;
-_putchar('\n');
+_putchar(alphabet);
+r('\n');
+
 }
+}
+
