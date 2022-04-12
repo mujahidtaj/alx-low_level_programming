d6563b3eaa2bb01509851de440baf3797d9e1d1c
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-04-12 11:46:20 -0700

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_sign - return 0 letter not lowercase, 1 letter lowercase
+ *@c: The int to print
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar ('+');
+return (1);
+}
+else
+{
+_putchar ('0')
+return (0);
+}
+else
+{
+_putchar ('-');
+return (-1);
+}
+}
