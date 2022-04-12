feae1f70866313f00a6550c2257d075d75efd7cc
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-04-12 06:05:56 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ 
+  * _islower - Short description, single line
+  *
+  *  * @c: contains value to be compared
+ */
+nt _islower(int c)
+{
+if (c > 'a' && c < 'z')
+{
+return (1);
+}
+else
+{
+return(0)
+}
+}
+
+
