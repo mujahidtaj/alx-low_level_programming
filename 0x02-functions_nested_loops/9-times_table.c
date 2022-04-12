6db43b7ea6fd00d041b6c870dd39350491558b2d
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-04-12 16:23:06 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,55 +14,30 @@ for (a = 0; a <= 9; a++)
 for (b = 0; b <= 9; b++)
 {
 c = a * b;
-
 if ((c / 10) == 0)
-
 {
-
 if (b == 0)
-
 {
-
 _putchar ('0');
-
 }
-
 if (b != 0)
-
 {
-
 _putchar (' ');
-
 _putchar ((c % 10) + '0');
-
-}
-
-if (b < 9)
-
 {
-
+if (b < 9)
+{
 _putchar(',');
-
 _putchar (' ');
-
 }
-
 }
-
 else
-
 {
-
 _putchar ((c / 10) + '0');
-
 	
 _putchar ((c % 10) + '0');
-
-if (b < 9)
-
-	
+if (b < 9)	
 {
-
 _putchar(',');
 
 _putchar (' ');
@@ -72,9 +47,7 @@ _putchar (' ');
 }
 
 }
-
 _putchar ('\n');
-
 }
 
 }
