d077d0edcd7716f6f865d256658fb20fb5be930c
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-04-14 18:21:11 -0700

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
-/** 
+/**
  * _isupper - checks for uppercase character
  *  @letter: the character to be tracked
- *  Return: 1 if c is uppercase, 0 otherwise
+ *   Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int letter)
 {
