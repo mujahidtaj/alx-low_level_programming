246c618192b3de327cf49d0fb9451cd7a95d37e8
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-04-15 14:09:37 -0700

    12. Numbers have life

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,7 +4,7 @@
  * print_number - prints an integer
  * @n: tracked var
  */
-void print_numbers(int n)
+void print_number(int n)
 {
 unsigned int x = n;
 if (n < 0)
@@ -14,7 +14,7 @@ x = -x;
 }
 if ((x / 10) > 0)
 {
-print_numbers(x / 10);
+print_number(x / 10);
 }
 _putchar((x % 10) + 48);
 
