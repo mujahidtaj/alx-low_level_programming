5e63d7dbe922b94816ead7118fa56dfc9ff55eb3
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-04-20 06:25:34 -0700

    Half the lies

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other character
+ * @str: pointer to char
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+void puts2(char *str)
+while (str[i] != '\0')
+{
+_putchar(*(str + i));
+}
+i += 1;
+}
+_putchar('\n');
+}
