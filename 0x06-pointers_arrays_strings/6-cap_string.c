dd7b15a1a0aa50d74423e0dd5503d10b4e922889
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-04-24 02:03:12 -0700

    6. Expect the best

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,7 +6,7 @@
  *   @s: ...
  *   Return: char value
  */
-char *cap_string(char *s)
+char *cap_string(char *)
 {
 int a = 0, i;
 int cspc = 13;
@@ -14,7 +14,7 @@ char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
 while (s[a])
 {
 i = 0;
-while i < cspc)
+while (i < cspc)
 {
 if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
 s[a] -= 32;
@@ -24,5 +24,3 @@ a++;
 }
 return (s);
 }
-
-
