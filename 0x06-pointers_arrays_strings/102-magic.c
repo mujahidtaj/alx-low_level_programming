fc4bff236d9dca732943392b208829171a668351
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-04-24 16:41:51 -0700

    last

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
 
-/**
- * write your line of code here.
- * you are not allowed to use a
- */
 int main(void)
 {
 int n;
@@ -13,7 +9,6 @@ a[2] = 1024;
 p = &n;
 *(p + (sizeof(n) + 1)) = 98;
 printf("a[2] = %d\n", a[2]);
-return (0);
+ return (0)
 }
-
 
