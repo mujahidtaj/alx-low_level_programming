91420d28807d65001e3c487ce8e38a179fe3e88c
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-04-27 13:47:17 -0700

    3. strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- * strspn - search a string for a set of bytes\
- * @s: source string
- * @accept: accepted string
- * Return: number of bytes in the init segment
+ * _strspn - a function that gets the length of a prefix substring
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
  */
 unsigned int _strspn(char *s, char *accept)
 {
