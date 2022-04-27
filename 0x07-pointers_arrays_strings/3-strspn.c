677ccdf8c8b80a8f25a8345183425f80d55fd4f6
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-04-27 13:24:47 -0700

    3. strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,19 +9,18 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int a = 0, b, t = 0;
-while (accept[a])
+unsigned int i, n, value, check;
+value = 0;
+for (i = 0; s[i] != '\0'; i++)
 {
-b = 0;
-while (s[b] != 32)
+if (accept[n] == s[i])
 {
-if (accept[a] == s[b])
-{
-t++;
+value++;
+check = 1;
 }
-b++;
 }
-a++;
+if (check == 0)
+return (value);
 }
-return (t);
+return (value);
 }
