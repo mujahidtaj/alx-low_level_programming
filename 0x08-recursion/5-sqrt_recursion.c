4dd1c77aa281ecdc2514c5cb43ff56b4702f45e2
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-04-28 16:42:45 -0700

    Your subconscious

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,6 +6,7 @@
  *  @a: same number as n
  *  @b: number that iterates from 1 to n
  *  return: On success 1.
+ *  Return: -1 or sqrt of c
  *  On error, -1 is returned, and errno is set appropriately.
  */
 int sqrt2(int a, int b)
@@ -17,6 +18,7 @@ return (-1);
 return (sqrt2(a, b + 1));
 }
 /**
+ * _sqrt_recursion - returns the natural square root of a number
  * sqrt_recursion - returns the natural square root of n
  * @n: Number Integer
  * Return: On success 1.
