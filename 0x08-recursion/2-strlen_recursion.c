4e21e4ff3376b417b7bf39078405ad486c6793ec
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-04-28 16:12:33 -0700

    2dream

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - Returns Length of String
+ * @s: string
+ * @s: string
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _strlen_recursion(char *s)
+{
+if (*s != '\0')
+{
+return (1 + _strlen_recursion(s + 1));
+}
+return (0);
+}
