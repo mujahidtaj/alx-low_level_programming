577cd9acb867a6642d5c19873a8bd322e0f2e650
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-05-07 13:45:42 -0700

    5. Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -35,6 +35,12 @@ count++;
 }
 return (count);
 }
+/**
+ * main - Takes in exactly one argument for minimum coin count
+ * @argc: Number of command line arguments
+ *  @argv: Array name
+ *  Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
+ */
 int main(int argc, char *argv[])
 {
 int i, coin;
