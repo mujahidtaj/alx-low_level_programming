a047ca7105c7b94f301a41297881acada546fe53
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-05-07 13:14:33 -0700

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,19 +7,22 @@
  *  @argc: arguement count
  *   @argv: array of pointers to arguement strings
  *   Return: result of multiplication or 1
- */
+*/
 int main(int argc, char *argv[])
 {
-int a, b, result;
-if (argc <= 2)
+int i, j;
+if (argc == 1 || argc == 2)
 {
 printf("Error\n");
 return (1);
 }
-/*atoi convert a string to an int*/
-a = atoi(argv[1]);
-b = atoi(argv[2]);
-result = a * b;
-printf("%d\n", result);
+else
+{
+j = 1;
+for (i = 1; i < 3; i++)
+j *= atoi(argv[i]);
+printf("%d\n", j);
+}
 return (0);
 }
+
