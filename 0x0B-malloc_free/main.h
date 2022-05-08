5384a8caeee9bb2d05cb55aa32989daa05c04a84
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-05-08 02:57:22 -0700

    0. Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,10 +1,12 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 int _putchar(char c);
-void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+int _strlen(char *s);
+char *str_concat(char *s1, char *s2);
+char *_strncat(char *dest, char *src, int n);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
 #endif
