1fa44b9c644e430bd1677ab71f9447997f0ff87a
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-05-08 03:05:07 -0700

    1. The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - return length of a string
+ * @s: char type
+ * Return:  length of string
+ *
+ * @size: unsigned int type
+ * @c: char type
+ * Return: Return pointer to array created
+ */
+int _strlen(char *s)
+{
+int a;
+for (a = 0; s[a] != '\0'; a++)
+{
+}
+return (a);
+}
+char *_strdup(char *str)
+{
+char *ptr;
+int size;
+int x;
+if (str == NULL)
+{
+return (NULL);
+}
+size = _strlen(str) + 1;
+ptr = malloc(size * sizeof(char));
+x = 0;
+while (x < size)
+{
+if (ptr == NULL)
+{
+return (NULL);
+}
+ptr[x] = str[x];
+x++;
+}
+return (ptr);
+}
+
