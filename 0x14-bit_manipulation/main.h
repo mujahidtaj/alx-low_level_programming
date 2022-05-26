3d053f6fd9f1ce6aaadf483d504d8ff48c866ec5
Mujahid taj <mujahid.taj@nadc.gov.sd>
2022-05-26 08:24:16 -0700

    modify main

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -10,5 +10,6 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 int _putchar(char c);
+int _strlen(const char *s)
 
 #endif
